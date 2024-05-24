f80baa42dab2837f8f8f90d7c1930dd8c2fe8033
luismagnum <luismagnum1@gmail.com>
2024-05-24 00:28:14 -0300

    change home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,15 @@
 import React from 'react'
-import Button from '../layouts/Button';
 
 const Home = () => {
   return (
-    <div className='min-h-screen flex flex-row justify-between items-center lg:px-32 px-5 bg-[url(https://media.istockphoto.com/id/1461055469/es/foto/teque%C3%B1os-o-dedos-de-queso.jpg?s=612x612&w=0&k=20&c=bYU-KNXETU6zkFQvB92F5eJZMXZdtJh08MoXcZgDjVU=)] bg-cover bg-no-repeat'>
+    <div className='w-full h-80  md:min-h-screen  flex flex-col justify-center items-center lg:px-32 px-5 bg-[url(https://media.istockphoto.com/id/1280159987/es/foto/empanadas-or-empanadillas.jpg?s=612x612&w=0&k=20&c=ImyJmRnWGQWZBXYhaz8S0ebnDVns8x3cA9KQsjLKVVo=)] bg-contain md:bg-cover bg-no-repeat wfull'>
       <div className='w-full lg:w-2/3 space-y-5'>
-        <h1 className='text-red-800 font-bold text-4xl'>Empanadas tequeños y pastelitos venezolanos</h1>
-        <p className='text-white text-xl'>
+        <h1 className='text-red-800 font-bold text-xl md:text-4xl'></h1>
+        <p className='hidden:sm text-white text-sm md:text-xl font-bold'>
             Las Empanadas Venezolanas son un envoltorio de masa de maiz con un relleno
             que puede ser de carne, pollo o queso cuya coccion es frita.
         </p>
-        <div className='lg:pl-44'>
-           <button className='border-2 border-black bg-black p-2 rounded-full text-white'>
-             <a href="https://wa.me/542984792639?text=Hola quiero hacer un pedido " target="_blank">Encargar ahora</a>
-           </button>
-       </div>
+        
       </div>
     </div>
   )
--- src/components/Productos.jsx
+++ src/components/Productos.jsx
@@ -3,15 +3,14 @@ import DishesCard from '../layouts/DishesCard'
 import img1 from "../image/im1.png"
 import img2 from "../image/im2.png"
 import img3 from "../image/im3.png"
-import img4 from "../image/im4.png"
 import img5 from "../image/im5.png"
 import img6 from "../image/im6.png"
 import img13 from "../image/img13.png"
 
 const Productos = () => {
   return (
-    <div className='min-h-screenflex flex-col justify-center items-center lg:px-32 px-5 '>
-      <h1 className='text-4xl font-bold text-center pt-24 pb-10'>Nuestras Comidas</h1>
+    <div className='min-h-screen flex flex-col justify-center items-center lg:px-32 px-5 '>
+      <h1 className='text-4xl font-bold text-center pt-2 md:pt-14 pb-10'>Nuestras Comidas</h1>
       <div className='flex flex-wrap gap-8 justify-center'>
         <DishesCard img={img1} title="empanadas 12u" price="$12000" />
         <DishesCard img={img2} title="tequeños 12u" price="$8000" />
