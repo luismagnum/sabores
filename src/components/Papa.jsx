086341893808de3ffddddcd1e06a6ad934c48cc8
luismagnum <luismagnum1@gmail.com>
2024-06-27 23:49:46 -0300

    text color

--- src/components/Papa.jsx
+++ src/components/Papa.jsx
@@ -4,9 +4,9 @@ const Home = () => {
   return (
     <div className=' w-full h-80 md:min-h-screen flex flex-col justify-center items-center lg:px-32 px-5 bg-[url(https://media.istockphoto.com/id/491098592/es/foto/pastel-polenta-frito.jpg?s=612x612&w=0&k=20&c=gOu9f4VSMncA9x8fbavdTU48PQjuwZAcjZyz5QZd81g=)] bg-contain md:bg-cover bg-no-repeat wfull'>
       <div className='mb-20'>
-        <h1 className='text-[#250608] text-xl  font-bold  md:text-5xl'>Empanada Condor Blue</h1>
-        <p className='text-base text-[#1a0f04] md:text-4xl font-bold'>
-          pollo <span className='text-sm block text-[#0b2d47] md:text-4xl'>Queso, Jamon y Queso azul</span>
+        <h1 className='text-[#02182b] text-xl  font-bold  md:text-5xl'>Empanada Condor Blue</h1>
+        <p className='text-xl text-[#fcc666] md:text-4xl font-bold'>
+          pollo <span className='text-xl block text-[#a90206] md:text-4xl'>Queso, Jamon y Queso azul</span>
         </p>
       </div>
     </div>
