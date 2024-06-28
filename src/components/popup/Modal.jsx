f239cf62263f12adcfb3237275c5a26a04c6c94e
luismagnum <luismagnum1@gmail.com>
2024-06-27 23:55:09 -0300

    modal

--- src/components/popup/Modal.jsx
+++ src/components/popup/Modal.jsx
@@ -5,7 +5,7 @@ const Modal = ({ isOpen, onClose, children }) => {
 
   return (
     <div className={`fixed z-10 inset-0 overflow-y-auto ${modalClass}`}>
-      <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+      <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-10 text-center sm:block sm:p-0">
         <div className="fixed inset-0 transition-opacity" aria-hidden="true">
           <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
         </div>
