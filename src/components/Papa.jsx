1a602ed6f4e8b894b8d568a406a46399a0cdd1ea
luismagnum <luismagnum1@gmail.com>
2024-06-30 23:16:22 -0300

    modal

--- src/components/Papa.jsx
+++ src/components/Papa.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     <div className=' w-full h-80 md:min-h-screen flex flex-col justify-center items-center lg:px-32 px-5 bg-[url(https://media.istockphoto.com/id/491098592/es/foto/pastel-polenta-frito.jpg?s=612x612&w=0&k=20&c=gOu9f4VSMncA9x8fbavdTU48PQjuwZAcjZyz5QZd81g=)] bg-contain md:bg-cover bg-no-repeat wfull'>
       <div className='mb-20'>
         <h1 className='text-[#02182b] text-xl  font-bold  md:text-5xl'>Empanada Condor Blue</h1>
-        <p className='text-xl text-[#fcc666] md:text-4xl font-bold'>
+        <p className='text-xl text-[#02182b] md:text-4xl font-bold'>
           pollo <span className='text-xl block text-[#a90206] md:text-4xl'>Queso, Jamon y Queso azul</span>
         </p>
       </div>
