2c845150506d7c6c39f723076b27ff7482acfe54
luismagnum <luismagnum1@gmail.com>
2024-07-09 18:34:49 -0300

    imagen ppal

--- src/components/Papa.jsx
+++ src/components/Papa.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className=' w-full h-80 md:min-h-screen flex flex-col justify-center items-center lg:px-32 px-5 bg-[url(https://thumbs.dreamstime.com/b/puma-cumbre-official-ball-conmebol-copa-america-soccer-miami-florida-united-states-may-317641402.jpg)] bg-contain md:bg-cover bg-no-repeat wfull'>
+    <div className=' w-full h-80 md:min-h-screen flex flex-col justify-center items-center lg:px-32 px-5 bg-[url(https://media.istockphoto.com/id/1402776309/es/foto/empanadas-fritas.jpg?s=612x612&w=0&k=20&c=VRk2FXIefnM6MZcnyxuGmLBDF7zZAhaNgh14qhbFEE0=)] bg-contain md:bg-cover bg-no-repeat wfull'>
       <div className='mb-20'>
-        <h1 className='text-[#f8be1e] text-xl  font-bold  md:text-5xl'>Empanada Condor Blue</h1>
+        <h1 className='text-[#000] text-xl  font-bold  md:text-5xl'>Empanada Condor Blue</h1>
         <p className='text-xl text-[#26599c] md:text-4xl font-bold'>
           Pollo <span className='text-xl block text-[#a90206] md:text-4xl'>Queso y Jamon.</span>
         </p>
