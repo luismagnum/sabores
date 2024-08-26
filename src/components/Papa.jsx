7d528167d3e612f7f0bb17a77ff8fa8e5703c422
luismagnum <luismagnum1@gmail.com>
2024-08-25 23:23:28 -0300

    change title

--- src/components/Papa.jsx
+++ src/components/Papa.jsx
@@ -4,9 +4,9 @@ const Home = () => {
   return (
     <div className=' w-full h-80 md:min-h-screen flex flex-col justify-center items-center lg:px-32 px-5 bg-[url(https://media.istockphoto.com/id/1402776309/es/foto/empanadas-fritas.jpg?s=612x612&w=0&k=20&c=VRk2FXIefnM6MZcnyxuGmLBDF7zZAhaNgh14qhbFEE0=)] bg-contain md:bg-cover bg-no-repeat wfull'>
       <div className='mb-20'>
-        <h1 className='text-[#fff] text-xl  font-bold  md:text-5xl'>Empanada Condor Blue</h1>
+        <h1 className='text-[#fff] text-xl  font-bold  md:text-5xl'>Ricas Empanadas</h1>
         <p className='text-xl text-[#fff] md:text-4xl font-bold'>
-          Pollo <span className='text-xl block text-[#fff] md:text-4xl'>Queso y Jamon.</span>
+          Y deliciosos<span className='text-xl block text-[#fff] md:text-4xl'>Tequeños</span>
         </p>
       </div>
     </div>
